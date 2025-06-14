b1a484b6c4db63f9771d80cdf6b3576b81464542
6ill <kyrbill123@gmail.com>
2025-06-14 21:12:40 +0700

    fix/update fuel logs path in  edit form

--- resources/views/admin/fuel-logs/edit.blade.php
+++ resources/views/admin/fuel-logs/edit.blade.php
@@ -1,7 +1,7 @@
 <x-layouts.admin>
     <x-slot:title>Tambah Log BBM untuk: {{ $vehicle->name }}</x-slot:title>
 
-    <form action="{{ route('admin.vehicles.fuel-logs.store', $fuelLog) }}" method="POST">
+    <form action="{{ route('admin.fuel-logs.update', $fuelLog) }}" method="POST">
         @csrf
         @method('PUT')
         @include('admin.fuel-logs.partials._form', ['log' => $fuelLog])
