4c407ec30128b377bb11907f281c1a73c5866050
6ill <kyrbill123@gmail.com>
2025-06-15 11:02:39 +0700

    fix/users migration and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class,
             LocationSeeder::class,
+            UserSeeder::class,
             EmployeeSeeder::class,
             VehicleSeeder::class,
             DriverSeeder::class,
